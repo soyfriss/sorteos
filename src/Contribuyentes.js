3f7a53e78339d011b02960621465944b30726835
soyfriss <federico.rissone@gmail.com>
2022-11-03 14:24:47 -0300

    update Contribuyentes component

--- src/Contribuyentes.js
+++ src/Contribuyentes.js
@@ -1,21 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Contribuyente from "./Contribuyente";
 
 const Contribuyentes = (props) => {
-    useEffect(() => {
-        console.log('Renderizando Contribuyentes');
-        console.log(props.contribuyentes.length);
-    }, [props.contribuyentes]);
+    const [conDobleChance, setConDobleChance] = useState(0);
+    const [conMasDeDosChances, setConMasDeDosChances] = useState(0);
 
     const getTotalContribuyentesConChance = (chanches) => {
         return props.contribuyentes.filter(c => c.chances === chanches).length;
     }
 
+    const getTotalContribuyentesConMasDeDosChances = () => {
+        return props.contribuyentes.filter(c => c.chances > 2).length;
+    }
+
+    useEffect(() => {
+        setConDobleChance(getTotalContribuyentesConChance(2));
+        setConMasDeDosChances(getTotalContribuyentesConMasDeDosChances());
+    });
+
+    useEffect(() => {
+        console.log('Renderizando Contribuyentes');
+        console.log(props.contribuyentes.length);
+    }, [props.contribuyentes]);
+
     return (
         <>
             <div>
                 <p>Contribuyentes: {props.contribuyentes.length}</p>
-                <p>Con doble chance: {getTotalContribuyentesConChance(2)}</p>
+                <p>Con doble chance: {conDobleChance}</p>
+                {conMasDeDosChances ? <p>Con más de dos chances: {conMasDeDosChances}</p> : ''}
             </div>
             <div>
                 {props.contribuyentes && props.contribuyentes.map(c => <Contribuyente key={c.partida} partida={c.partida} propietario={c.propietario} direccion={c.direccion} chances={c.chances} />)}
