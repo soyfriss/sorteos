c929502c081a1b4a05da2962e3140a1954006d22
soyfriss <federico.rissone@gmail.com>
2022-11-05 10:50:35 -0300

    Enhance Contribuyentes Component

--- src/Contribuyentes.js
+++ src/Contribuyentes.js
@@ -2,30 +2,45 @@ import React from "react";
 import Contribuyente from "./Contribuyente";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
+import Badge from "react-bootstrap/Badge";
 
 const Contribuyentes = (props) => {
 
+    const getTotalContribuyentes = () => {
+        const totalContribuyentes = props.contribuyentes.length;
+
+        return <Badge bg={totalContribuyentes ? 'primary' : 'danger'}>{totalContribuyentes}</Badge>;
+    };
+
     const getTotalContribuyentesConChance = (chanches) => {
-        if (props.contribuyentes) return props.contribuyentes.filter(c => c.chances === chanches).length;
-        return 0;
-    }; 
+        const totalContribuyentes = props.contribuyentes.filter(c => c.chances === chanches).length;
+
+        return <Badge bg={totalContribuyentes ? 'primary' : 'secondary'}>{totalContribuyentes}</Badge>;
+    };
 
     const getTotalContribuyentesConMasDeDosChances = () => {
-        if (props.contribuyentes) return props.contribuyentes.filter(c => c.chances > 2).length;
-        return 0;
+        const totalContribuyentes = props.contribuyentes.filter(c => c.chances > 2).length;
+
+        return <Badge bg={totalContribuyentes ? 'primary' : 'secondary'}>{totalContribuyentes}</Badge>;
     };
 
     let button = '';
-    if(props.contribuyentes.length) {
+    if (props.contribuyentes.length) {
         button = <Button onClick={() => props.sortear()}>Sortear</Button>;
     }
 
     return (
         <>
             <div>
-                <Alert variant={props.contribuyentes.length ? 'success' : 'danger'}>Contribuyentes: {props.contribuyentes.length}</Alert>
-                <Alert variant="secondary">Con doble chance: {getTotalContribuyentesConChance(2)}</Alert>
-                <Alert variant="secondary">Con más de dos chances: {getTotalContribuyentesConMasDeDosChances()}</Alert>
+                <h5>
+                    <Alert variant={props.contribuyentes.length ? 'success' : 'danger'}>Contribuyentes: {getTotalContribuyentes()}</Alert>
+                </h5>
+                <h5>
+                    <Alert variant="secondary">Con doble chance: {getTotalContribuyentesConChance(2)}</Alert>
+                </h5>
+                <h5>
+                    <Alert variant="secondary">Con más de dos chances: {getTotalContribuyentesConMasDeDosChances()}</Alert>
+                </h5>
                 {button}
             </div>
             <div className="lista-contribuyentes">
