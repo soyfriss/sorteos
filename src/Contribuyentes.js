6ff269218e866d6996c95b661833c69dd0613afb
soyfriss <federico.rissone@gmail.com>
2022-11-07 09:41:46 -0300

    Change Alerts to Accordions

--- src/Contribuyentes.js
+++ src/Contribuyentes.js
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import Badge from "react-bootstrap/Badge";
+import Accordion from "react-bootstrap/Accordion";
+import AccordionItem from "react-bootstrap/esm/AccordionItem";
 
 const Contribuyentes = (props) => {
 
@@ -27,14 +29,30 @@ const Contribuyentes = (props) => {
     
     let button = '';
     if (props.name && props.contribuyentes.length && !winners.length) {
-        button = <Button size="lg" onClick={() => props.sortear()}>Sortear</Button>;
+        button = <Button size="lg" className="mt-3 mx-3" onClick={() => props.sortear()}>Sortear</Button>;
     }
+
+    let resetButton = <Button size="lg" className="mt-3" onClick={() => props.reset()}>Reiniciar</Button>;
 
     if (props.contribuyentes.length) {
         return (
             <>
                 <div>
-                    <h5>
+                    <Accordion alwaysOpen>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header><h5>Contribuyentes: {getTotalContribuyentes()}</h5></Accordion.Header>
+                            <Accordion.Body>Tabla de contribuyentes</Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header><h5>Con doble chance: {getTotalContribuyentesConChance(2)}</h5></Accordion.Header>
+                            <Accordion.Body>Tabla de contribuyentes con doble chance</Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header><h5>Con más de dos chances: {getTotalContribuyentesConMasDeDosChances()}</h5></Accordion.Header>
+                            <Accordion.Body>Tabla de contribuyentes con más de dos chances</Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                    {/* <h5>
                         <Alert variant={props.contribuyentes.length ? 'success' : 'danger'}>Contribuyentes: {getTotalContribuyentes()}</Alert>
                     </h5>
                     <h5>
@@ -42,7 +60,8 @@ const Contribuyentes = (props) => {
                     </h5>
                     <h5>
                         <Alert variant="secondary">Con más de dos chances: {getTotalContribuyentesConMasDeDosChances()}</Alert>
-                    </h5>
+                    </h5> */}
+                    {resetButton}
                     {button}
                 </div>
 
