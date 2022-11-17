db2b6be5cad4d3943d1fa9e885e0e698ecc5ceb3
soyfriss <federico.rissone@gmail.com>
2022-11-17 16:00:35 -0300

    Create action types

---
+++ src/redux/actions.js
@@ -0,0 +1,6 @@
+export const GET_ENTRANTS = "GET_ENTRANTS";
+export const EDIT_ENTRANTS = "EDIT_ENTRANTS";
+export const CREATE_RAFFLE = "CREATE_RAFFLE";
+export const RUN_RAFFLE = "RUN_RAFFLE";
+
+
