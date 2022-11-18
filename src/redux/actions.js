7d290dd07a744158c8bb608bbde8022dca13baca
soyfriss <federico.rissone@gmail.com>
2022-11-17 22:23:33 -0300

    Add redux files

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,30 @@
 export const GET_ENTRANTS = "GET_ENTRANTS";
-export const EDIT_ENTRANTS = "EDIT_ENTRANTS";
-export const CREATE_RAFFLE = "CREATE_RAFFLE";
-export const RUN_RAFFLE = "RUN_RAFFLE";
+export const ADD_ENTRANTS = "ADD_ENTRANTS";
+export const SET_RAFFLE_NAME = "SET_RAFFLE_NAME";
+export const SET_RAFFLE_NUMBER_OF_WINNERS = "SET_RAFFLE_NUMBER_OF_WINNERS";
+// export const RUN_RAFFLE = "RUN_RAFFLE";
 
+export const getEntrants = () => {
+    return {type: GET_ENTRANTS};
+}
 
+export const addEntrants = (entrants) => {
+    return {
+        type: ADD_ENTRANTS,
+        payload: entrants
+    }
+}
+
+export const setRaffleName = (name) => {
+    return {
+        type: SET_RAFFLE_NAME,
+        payload: name
+    }
+}
+
+export const setRaffleNumberOfWinners = (numberOfWinners) => {
+    return {
+        type: SET_RAFFLE_NUMBER_OF_WINNERS,
+        payload: numberOfWinners
+    }
+}
